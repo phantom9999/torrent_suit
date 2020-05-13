cb6c35c0e85c42b505043391f02d2cf2ccc5e001
phantom9999 <1057369361@qq.com>
2020-05-13 23:34:05 +0800

    fix

---
+++ action_pre.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+set -e
+
+zypper install -y libboost_system1_66_0-devel libboost_log1_66_0-devel libboost_filesystem1_66_0-devel libevent-devel sqlite3-devel libthrift-devel hiredis-devel protobuf-devel snappy-devel log4cpp-devel libopenssl-devel cmake make thrift
+
+
