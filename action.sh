cbf0c30045502fd7bd5b7b403773f31d9f09d61d
phantom9999 <1057369361@qq.com>
2020-05-15 21:14:54 +0800

    add

--- action.sh
+++ action.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 set -e
 
+RUN zypper install -y boost_1_58_0-devel libevent-devel sqlite3-devel libthrift-devel hiredis-devel protobuf-devel snappy-devel log4cpp-devel libopenssl-devel cmake make thrift
+
 sh proto_gen.sh ;
 cmake . -DBOOST_TEST_DYN_LINK=1; 
 make -j4
