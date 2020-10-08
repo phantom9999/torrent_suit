5382f0891bd187be42fc77ca7789596be9e59154
phantom9999 <1057369361@qq.com>
2020-10-08 19:06:43 +0800

    update action

--- action.sh
+++ action.sh
@@ -1,19 +1,7 @@
 #! /bin/bash
 set -x
 
-zypper install -y \
-    boost_1_58_0-devel \
-    libevent-devel \
-    sqlite3-devel \
-    libthrift-devel \
-    hiredis-devel \
-    protobuf-devel \
-    snappy-devel \
-    log4cpp-devel \
-    libopenssl-devel \
-    cmake make thrift
-
 sh proto_gen.sh ;
-cmake . -DBOOST_TEST_DYN_LINK=1; 
+cmake .;
 make -j4
 
